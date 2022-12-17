d952724d3fd625d77b77b5bce5b3d8280cf60cb0
Jose Carlos <josecarloscjj@gmail.com>
2022-12-17 07:58:55 -0300

    Finished challenge one

--- primeiroDesafio/src/Main.java
+++ primeiroDesafio/src/Main.java
@@ -1,5 +1,42 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    List<Integer> numeros = new ArrayList<>();
+
+    int numero = sc.nextInt();
+    while (numero <= 0) {
+      System.out.println("Numero inválido, digite um novo número: ");
+      numero = sc.nextInt();
     }
+
+    for (int i = 0; i < numero; i++) {
+      int n = sc.nextInt();
+      numeros.add(n);
+    }
+
+    List<Integer> numerosPares = numeros.stream()
+            .filter(n -> n % 2 == 0)
+            .sorted()
+            .collect(Collectors.toList());
+
+    List<Integer> numerosImpares =  numeros.stream()
+            .filter(n -> n % 2 == 1)
+            .sorted(Comparator.reverseOrder())
+            .collect(Collectors.toList());
+
+    numeros.clear();
+    numeros.addAll(numerosPares);
+    numeros.addAll(numerosImpares);
+
+    numeros.forEach(System.out::println);
+
+    sc.close();
+  }
 }
