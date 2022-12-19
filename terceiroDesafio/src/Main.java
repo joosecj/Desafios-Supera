446106236fb7961fb5cc20d4e3281bc457fc6f4f
Jose Carlos <josecarloscjj@gmail.com>
2022-12-19 14:40:23 -0300

    Finished challenge three

--- terceiroDesafio/src/Main.java
+++ terceiroDesafio/src/Main.java
@@ -1,5 +1,30 @@
+import java.util.*;
+
 public class Main {
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    List<Integer> numeros = new ArrayList<>();
 
+    int n = sc.nextInt();
+    int k = sc.nextInt();
+    for (int i = 0; i < n; i++) {
+      numeros.add(sc.nextInt());
+    }
+    System.out.printf("\n%d", verificarNumero(numeros, k));
+    sc.close();
+  }
+  private static int verificarNumero(List<Integer> numeros, int valorAlvo) {
+    int result = 0;
+    for(int i = 0; i < numeros.size(); i++) {
+      int posicaoUm = numeros.get(i);
+      for (int j = i+1; j < numeros.size(); j++) {
+        int posicaoDois = numeros.get(j);
+        int subtracao = (posicaoUm > posicaoDois ? posicaoUm - posicaoDois : (posicaoUm - posicaoDois) * -1);
+        if(subtracao == valorAlvo) {
+          result++;
+        }
+      }
+    }
+    return result;
   }
 }
