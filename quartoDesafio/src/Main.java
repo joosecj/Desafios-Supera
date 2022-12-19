6d8e7583493d4954d8af2008d66a799bd003704b
Jose Carlos <josecarloscjj@gmail.com>
2022-12-19 15:16:51 -0300

    Finished challenge fourth

--- quartoDesafio/src/Main.java
+++ quartoDesafio/src/Main.java
@@ -1,5 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Scanner sc = new Scanner(System.in);
+    List<String> texto = new ArrayList<>();
+
+    int N = sc.nextInt();
+    for (int i = 0; i <= N; i++) {
+      texto.add(decodificarLinha(sc.nextLine()));
+    }
+    texto.forEach(System.out::println);
+  }
+  private static String decodificarLinha(String line) {
+    StringBuilder parte1 = new StringBuilder(line.substring(0, line.length() / 2));
+    StringBuilder parte2 = new StringBuilder(line.substring(line.length() / 2));
+    parte1.reverse();
+    parte2.reverse();
+    return parte1.toString() + parte2;
   }
 }
